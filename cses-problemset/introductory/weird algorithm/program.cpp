300951151550521ab57dbb068ae210d5c47cf5a4
Ankit <ankitspare981@gmail.com>
2025-08-06 19:54:02 +0530

    019

---
+++ cses-problemset/introductory/weird algorithm/program.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long n;
+    cin>>n;
+    while(n!=1){
+        cout<<n<<" ";
+        n=(n%2)?(n*3+1):n/2;
+    }
+    cout<<1<<endl;
+}
