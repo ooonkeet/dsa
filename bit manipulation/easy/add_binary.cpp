9f9b0a6517346fff0198b1a5804d9bacae867a50
Ankit <ankitspare981@gmail.com>
2025-08-07 02:36:19 +0530

    001
    Basic start of bitwise operations implied in c++ programs

---
+++ bit manipulation/easy/add_binary.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+string addBinary(string a, string b){
+    string ans="";
+    int carry=0;
+    int i=a.length()-1,j=b.length()-1;
+    while(i>=0 || j>=0 || carry==1){
+        int sum=carry;
+        if(i>=0){
+            sum+=a[i]-'0';
+            i--;
+        }
+        if(j>=0){
+            sum+=b[j]-'0';
+            j--;
+        }
+        ans+=sum%2;
+        carry=sum/2;
+    }
+    return reverse(ans);
+}
+int main(){
