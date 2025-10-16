c7a1a3c0ec6a5f53b967ea13037a3a103dbee8ba
Ankit <ankitspare981@gmail.com>
2025-10-17 03:27:58 +0530

    046

---
+++ cses-problemset/sorting and searching/playlist/program.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, k, l, ans;
+map<int,int> mp;
+
+int main(){
+    //scanf("%d", &N);
+    cin>>N;
+    l = 1;
+    for(int r = 1; r <= N; r++){
+        cin>>k;
+        //scanf("%d", &k);
+        if(mp[k]){
+            ans = max(ans, r-l);
+            l = max(l, mp[k]+1);
+            mp[k] = r;
+        } else {
+            ans = max(ans, r-l+1);
+            mp[k] = r;
+        }
+    }
+
+    ans = max(N-l+1, ans);
+    //printf("%d\n", ans);
+    cout<<ans<<endl;
+}
