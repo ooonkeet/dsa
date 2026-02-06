d53932caa16ac91d02b83bb86840222fa2dd939a
Ankit <ankitspare981@gmail.com>
2026-02-07 02:27:51 +0530

    002

---
+++ dp/min cost climbing stairs/rec+memo.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int minCost(vector<int> cost,int i,vector<int> &dp){
+        if(i==0||i==1)
+            return cost[i];
+        if(dp[i]!=-1)
+            return dp[i];
+        return dp[i]=cost[i]+min(minCost(cost,i-1,dp),minCost(cost,i-2,dp));
+    }
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();
+        vector<int>dp(n,-1);
+        return min(minCost(cost,n-1,dp),minCost(cost,n-2,dp));
+    }
+};
