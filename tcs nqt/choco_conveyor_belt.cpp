7f0a74509e6f0baed08f5af96c03723d79c520a9
Ankit <ankitspare981@gmail.com>
2026-08-07 16:40:06 +0530

    tcs-1

---
+++ tcs nqt/choco_conveyor_belt.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,j=0;
+    cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++){
+        cin>>v[i];
+    }
+    for(int i=0;i<n;i++){
+        if(v[i]!=0){
+            swap(v[i],v[j]);
+            j++;
+        }
+    }
+    for(int i=0;i<n;i++){
+        cout<<v[i]<<" ";
+    }
+    return 0;
+}
