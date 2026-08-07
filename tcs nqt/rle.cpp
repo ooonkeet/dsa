dddcc02f8df86dc11603b46d7d3ad3f4b15d2c59
Ankit <ankitspare981@gmail.com>
2026-08-08 02:22:09 +0530

    tcs-3

---
+++ tcs nqt/rle.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string s,enc="";
+    cin>>s;
+    int n=s.length();
+    //sort(s.begin(),s.end());
+    for(int i=0;i<n;i++) {
+        int count=1;
+        while (i<n-1 && s[i]==s[i+1]) {
+            count++;
+            i++;
+        }
+        enc+=s[i];
+        enc+=to_string(count);
+    }
+    cout<<enc;
+    return 0;
+}
